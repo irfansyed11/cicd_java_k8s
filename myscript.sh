bcbb8d1250bbef429b5e6be0dd36563cdc5418aa
Syed Irfan <48002646+irfansyed11@users.noreply.github.com>
2024-05-11 01:09:19 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # set -x
 docker build -t mvnbuild -f Dockerfilenew .
-docker run -it -d mvnbuild bash
-contid="$(docker ps | grep mvnbuild | sed 's/|/ /' | awk '{print $1}')"
+contid="$(docker run -it -d mvnbuild bash)"
 echo $contid
 docker cp $contid:/app/target/gamutkart.war .
