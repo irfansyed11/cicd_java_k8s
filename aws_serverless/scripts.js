67f9a4d85e191cfec80558d14e321e9e5909031e
Syed Irfan <48002646+irfansyed11@users.noreply.github.com>
2024-06-11 12:31:54 +0530

    Update scripts.js

--- aws_serverless/scripts.js
+++ aws_serverless/scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "https://uu2qnmp4l9.execute-api.us-east-1.amazonaws.com/prod";
+var API_ENDPOINT = "https://uu2qnmp4l9.execute-api.us-east-1.amazonaws.com/prod/";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
